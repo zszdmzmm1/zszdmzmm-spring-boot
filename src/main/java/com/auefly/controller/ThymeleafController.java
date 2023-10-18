85c0d08af0298244bccb0fb539ee377f394fd2fd
zszdmzmm <1075534380@qq.com>
2023-10-19 01:42:48 +0800

    the first thymeleafPage

---
+++ src/main/java/com/auefly/controller/ThymeleafController.java
@@ -0,0 +1,14 @@
+package com.auefly.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ThymeleafController {
+    @GetMapping("/index")
+    public String index(Model model){
+        model.addAttribute("custom_name", "hjf");
+        return "index";
+    }
+}
