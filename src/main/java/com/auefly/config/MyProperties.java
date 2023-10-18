75b9445c30103d636b0625a543b53c8f7f66311e
zszdmzmm <1075534380@qq.com>
2023-10-18 16:23:33 +0800

    lombok

--- src/main/java/com/auefly/config/MyProperties.java
+++ src/main/java/com/auefly/config/MyProperties.java
@@ -1,36 +1,13 @@
 package com.auefly.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-
+@Data
 @ConfigurationProperties(
         prefix = "my"
 )
 public class MyProperties {
     private int age;
     private String name;
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "MyProperties{" +
-                "age=" + age +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
