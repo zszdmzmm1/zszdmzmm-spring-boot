898824be791ef1895654aaf76732117397a31a98
zszdmzmm <1075534380@qq.com>
2023-10-18 17:49:43 +0800

    测试properties与类绑定（映射）：被注入容器

--- src/test/java/com/auefly/MyFirstTest.java
+++ src/test/java/com/auefly/MyFirstTest.java
@@ -1,14 +1,18 @@
 package com.auefly;
 
+import com.auefly.config.MyProperties;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.UseMainMethod;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -17,11 +21,18 @@ public class MyFirstTest {
     @Test
     @DisplayName("第一个SpringBootTest: @AutoConfigureMockMvc mockMvc.perform()")
     void homePageTest(@Autowired MockMvc mockMvc) throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+        mockMvc.perform(get("/"))
                 .andExpect(status().is(200))
                 //equals to
                 .andExpect(status().isOk())
                 
                 .andExpect(content().string("Hello World!"));
+    }
+
+    @Test
+    @DisplayName("properties是否与自己写的类呈映射关系")
+    void exampleTest(@Autowired MyProperties myProperties) {
+        Assertions.assertEquals(myProperties.getAge(), 4);
+        Assertions.assertEquals(myProperties.getName(), "myName");
     }
 }
