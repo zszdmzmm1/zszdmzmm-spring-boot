8510518e704e07bb2ed2ab77395bdb9be19f9f99
zszdmzmm <1075534380@qq.com>
2023-10-18 11:40:01 +0800

    home page mapping -- hello world!

---
+++ src/main/java/com/auefly/controller/WebController.java
@@ -0,0 +1,12 @@
+package com.auefly.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+    @GetMapping("/")
+    String home(){
+        return "Hello World!";
+    }
+}
