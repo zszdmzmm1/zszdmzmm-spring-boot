dffb9a8e573464adade999e8697978dbeffc381f
zszdmzmm <1075534380@qq.com>
2023-10-19 12:03:02 +0800

    测试webjar引入bootstrap

---
+++ src/test/java/com/auefly/WebjarTest.java
@@ -0,0 +1,21 @@
+package com.auefly;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class WebjarTest {
+    @Test
+    @DisplayName("webjar引入bootstrap")
+    public void bootstrapTest(@Autowired MockMvc mockMvc) throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/webjars/bootstrap/5.3.0/css/bootstrap.min.css")).andExpect(status().isOk());
+    }
+}
