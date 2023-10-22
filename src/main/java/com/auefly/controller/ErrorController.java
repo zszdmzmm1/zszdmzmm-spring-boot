76f9e16bb99a09d6ae3f2b6da36451b9685ac66f
zszdmzmm <1075534380@qq.com>
2023-10-22 21:14:49 +0800

    自定义@responseStatus

--- src/main/java/com/auefly/controller/ErrorController.java
+++ src/main/java/com/auefly/controller/ErrorController.java
@@ -1,7 +1,10 @@
 package com.auefly.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.NOT_IMPLEMENTED;
 
 @RestController
 public class ErrorController {
@@ -9,5 +12,11 @@ public class ErrorController {
     public String servletError() {
         int i = 1 / 0;
         return String.valueOf(i);
+    }
+
+    @GetMapping("/custom-status")
+    @ResponseStatus(reason = "this is a custom error", code = NOT_IMPLEMENTED)
+    public String customStatus() {
+        return "this is a return of the custom error page";
     }
 }
