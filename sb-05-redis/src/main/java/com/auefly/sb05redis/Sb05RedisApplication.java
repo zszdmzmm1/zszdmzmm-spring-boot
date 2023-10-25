acff7bd260a0573afde376ed39823ac19ac173f4
zszdmzmm <1075534380@qq.com>
2023-10-25 08:29:39 +0800

    new sb-05-redis module

---
+++ sb-05-redis/src/main/java/com/auefly/sb05redis/Sb05RedisApplication.java
@@ -0,0 +1,13 @@
+package com.auefly.sb05redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb05RedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb05RedisApplication.class, args);
+    }
+
+}
---
+++ sb-05-redis/src/test/java/com/auefly/sb05redis/Sb05RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auefly.sb05redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb05RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
