dede01257298d65cb654c305d827aaacda670cb2
zszdmzmm <1075534380@qq.com>
2023-10-25 11:35:21 +0800

    redis RedisTemplate<Object, Object> Serializable

---
+++ sb-05-redis/src/main/java/com/auefly/sb05redis/bean/User.java
@@ -0,0 +1,15 @@
+package com.auefly.sb05redis.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+    private String name;
+    private String password;
+}
--- sb-05-redis/src/main/java/com/auefly/sb05redis/controller/HelloRedisController.java
+++ sb-05-redis/src/main/java/com/auefly/sb05redis/controller/HelloRedisController.java
@@ -1,6 +1,8 @@
 package com.auefly.sb05redis.controller;
 
+import com.auefly.sb05redis.bean.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,20 @@ public class HelloRedisController {
     @Autowired
     StringRedisTemplate stringRedisTemplate;
 
+    @Autowired
+    RedisTemplate<Object, Object> objectRedisTemplate;
+
     @GetMapping("hello-redis")
     public String helloRedis() {
         Long count = stringRedisTemplate.opsForValue().increment("count");
         return "count=" + count;
     }
+
+    @GetMapping("get-object2redis")
+    public User getObjectToRedis() {
+        User user = new User("John", "admin");
+        objectRedisTemplate.opsForValue().set("user", user);
+        return (User) objectRedisTemplate.opsForValue().get("user");
+    }
+
 }
