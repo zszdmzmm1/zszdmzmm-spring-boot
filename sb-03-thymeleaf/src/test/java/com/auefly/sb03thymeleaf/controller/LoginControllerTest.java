38fea1633dbf53d1abc97f1159dd6721972cd9d2
zszdmzmm <1075534380@qq.com>
2023-10-26 01:38:40 +0800

    thymeleaf: internationalization

--- sb-03-thymeleaf/src/test/java/com/auefly/sb03thymeleaf/controller/LoginControllerTest.java
+++ sb-03-thymeleaf/src/test/java/com/auefly/sb03thymeleaf/controller/LoginControllerTest.java
@@ -2,6 +2,7 @@ package com.auefly.sb03thymeleaf.controller;
 
 import org.hamcrest.Matchers;
 import org.hamcrest.core.StringContains;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -60,5 +63,28 @@ class LoginControllerTest {
                 .andExpect(MockMvcResultMatchers.request().sessionAttribute("user", Matchers.nullValue()))
                 .andExpect(MockMvcResultMatchers.redirectedUrl("/login"))
                 .andExpect(MockMvcResultMatchers.flash().attribute("msg", "请登录!"));
+    }
+
+    @Test
+    @DisplayName("测试国际化 - zh - 模拟浏览器切换语言为中文")
+    void i18nCN() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/login")
+                        .header("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8")
+                )
+                .andExpect(content().string(StringContains.containsString("邮箱")))
+        ;
+    }
+
+    @Test
+    @DisplayName("测试国际化 - en - 模拟浏览器切换语言为英文, 代码逻辑正确，但出现未知问题")
+    @Disabled
+    void i18nUS() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/login")
+                        .header("Accept-Language", "en")
+                )
+                .andExpect(content().string(StringContains.containsString("Email address")))
+        ;
     }
 }
