60448bb21e3a0e848e683764e5ae9bac9c039c08
zszdmzmm <1075534380@qq.com>
2023-10-25 11:39:22 +0800

    new sb-03-thymeleaf module

---
+++ sb-03-thymeleaf/src/main/java/com/auefly/sb03thymeleaf/Sb03ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.auefly.sb03thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb03ThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb03ThymeleafApplication.class, args);
+    }
+
+}
---
+++ sb-03-thymeleaf/src/test/java/com/auefly/sb03thymeleaf/Sb03ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auefly.sb03thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb03ThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
