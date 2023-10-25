91a721a601550ee2d138525e3028989e4bd7e974
zszdmzmm <1075534380@qq.com>
2023-10-26 04:21:03 +0800

    new sb-06-doc module

---
+++ sb-06-doc/src/main/java/com/auefly/sb06doc/Sb06DocApplication.java
@@ -0,0 +1,13 @@
+package com.auefly.sb06doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb06DocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb06DocApplication.class, args);
+    }
+
+}
---
+++ sb-06-doc/src/test/java/com/auefly/sb06doc/Sb06DocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auefly.sb06doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb06DocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
