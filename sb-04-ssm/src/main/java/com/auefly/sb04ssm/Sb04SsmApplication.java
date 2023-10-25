1132016c1d10c251013cd895d8586141cef186a8
zszdmzmm <1075534380@qq.com>
2023-10-26 01:58:53 +0800

    new sb-04-ssm module

---
+++ sb-04-ssm/src/main/java/com/auefly/sb04ssm/Sb04SsmApplication.java
@@ -0,0 +1,13 @@
+package com.auefly.sb04ssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb04SsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb04SsmApplication.class, args);
+    }
+
+}
---
+++ sb-04-ssm/src/test/java/com/auefly/sb04ssm/Sb04SsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auefly.sb04ssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb04SsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
