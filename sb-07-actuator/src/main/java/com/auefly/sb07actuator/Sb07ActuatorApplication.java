ccc8b0b269b09edf3e73b6a212ec13cf2a73907b
zszdmzmm <1075534380@qq.com>
2023-10-26 11:44:16 +0800

    new sb-07-actuator module

---
+++ sb-07-actuator/src/main/java/com/auefly/sb07actuator/Sb07ActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.auefly.sb07actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb07ActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb07ActuatorApplication.class, args);
+	}
+
+}
---
+++ sb-07-actuator/src/test/java/com/auefly/sb07actuator/Sb07ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auefly.sb07actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb07ActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
