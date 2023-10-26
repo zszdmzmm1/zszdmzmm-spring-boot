c594fbaeb694d6ca88651900658b61279c6574f6
zszdmzmm <1075534380@qq.com>
2023-10-26 14:25:09 +0800

    actuator test default set

--- sb-07-actuator/src/test/java/com/auefly/sb07actuator/Sb07ActuatorApplicationTests.java
+++ sb-07-actuator/src/test/java/com/auefly/sb07actuator/Sb07ActuatorApplicationTests.java
@@ -1,13 +1,37 @@
 package com.auefly.sb07actuator;
 
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class Sb07ActuatorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    MockMvc mockMvc;
 
+    @Test
+    @DisplayName("default配置， /actuator")
+    void defaultActuator() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/actuator"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("_links").value(Matchers.aMapWithSize(3)))
+                .andExpect(MockMvcResultMatchers.jsonPath("_links.self.href").value("http://localhost/actuator"))
+                .andExpect(MockMvcResultMatchers.jsonPath("_links.health.href").value("http://localhost/actuator/health"));
+    }
+
+    @Test
+    @DisplayName("default配置， /actuator/health")
+    void defaultActuatorHealth() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/actuator/health"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("status").value("UP"));
+    }
 }
