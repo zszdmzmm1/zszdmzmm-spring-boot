b41f86386e146d49a90d01b2613fdf7fb3586469
zszdmzmm <1075534380@qq.com>
2023-10-26 11:33:08 +0800

    template.setDefaultSerializer

---
+++ sb-05-redis/src/main/java/com/auefly/sb05redis/config/CustomRedisConfig.java
@@ -0,0 +1,18 @@
+package com.auefly.sb05redis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+
+@Configuration
+public class CustomRedisConfig {
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+}
