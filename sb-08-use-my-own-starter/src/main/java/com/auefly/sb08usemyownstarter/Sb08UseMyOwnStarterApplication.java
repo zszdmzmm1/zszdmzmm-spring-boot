7e0e1ac2c3092464df4dd315e1cdae20cb90e980
zszdmzmm <1075534380@qq.com>
2023-10-27 16:42:44 +0800

    new module use my own starter

---
+++ sb-08-use-my-own-starter/src/main/java/com/auefly/sb08usemyownstarter/Sb08UseMyOwnStarterApplication.java
@@ -0,0 +1,13 @@
+package com.auefly.sb08usemyownstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb08UseMyOwnStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb08UseMyOwnStarterApplication.class, args);
+    }
+
+}
---
+++ sb-08-use-my-own-starter/src/test/java/com/auefly/sb08usemyownstarter/Sb08UseMyOwnStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auefly.sb08usemyownstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb08UseMyOwnStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
