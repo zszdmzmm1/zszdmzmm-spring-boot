7bf0108cb781796a0e92a4a3ceaee5fceb4e113a
zszdmzmm <1075534380@qq.com>
2023-10-29 23:05:33 +0800

    use my own autoconfigure by import class

--- sb-08-use-my-own-starter/src/main/java/com/auefly/sb08usemyownstarter/Sb08UseMyOwnStarterApplication.java
+++ sb-08-use-my-own-starter/src/main/java/com/auefly/sb08usemyownstarter/Sb08UseMyOwnStarterApplication.java
@@ -1,9 +1,12 @@
 package com.auefly.sb08usemyownstarter;
 
+import com.auefly.spring.boot.autoconfigure.GreetingAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(GreetingAutoConfiguration.class)
 public class Sb08UseMyOwnStarterApplication {
 
     public static void main(String[] args) {
